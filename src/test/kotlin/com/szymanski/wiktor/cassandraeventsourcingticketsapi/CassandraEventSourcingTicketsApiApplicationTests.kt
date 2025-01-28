918e0586a71bb3e89416003ca6017bcdc732e92b
janm <metzler.infa@gmail.com>
2025-01-28 18:14:51 +0100

    dodanie mierzenia czasu

--- src/test/kotlin/com/szymanski/wiktor/cassandraeventsourcingticketsapi/CassandraEventSourcingTicketsApiApplicationTests.kt
+++ src/test/kotlin/com/szymanski/wiktor/cassandraeventsourcingticketsapi/CassandraEventSourcingTicketsApiApplicationTests.kt
@@ -12,86 +12,98 @@ class CassandraEventSourcingTicketsApiApplicationTests(addr : String) {
     private val api : TicketsAPI = TicketsAPI.create(apiAddr)
 
     fun createConcert() {
-        var response = api.createConcert(utils.generateEventName(), (5..25).random(), (10..100).random()).execute()
-        utils.checkValidity(response)
+        val timeTaken = measureTime {
+            var response = api.createConcert(utils.generateEventName(), (5..25).random(), (10..100).random()).execute()
+            utils.checkValidity(response)
 
-        val concertId = response.body()!!.string()
+            val concertId = response.body()!!.string()
 
-        response = api.getConcerts().execute()
-        utils.checkValidity(response)
-        assert(utils.containsValue(utils.getJsonArray(response), "id", concertId))
+            response = api.getConcerts().execute()
+            utils.checkValidity(response)
+            assert(utils.containsValue(utils.getJsonArray(response), "id", concertId))
+        }
+
+        println("Create test $timeTaken")
     }
 
     fun reserveSeat(){
-        var response = api.getConcerts().execute()
-        utils.checkValidity(response)
+        val timeTaken = measureTime {
+            var response = api.getConcerts().execute()
+            utils.checkValidity(response)
 
-        val concerts = utils.getJsonArray(response)
-        if (concerts.length() == 0)
-            return
+            val concerts = utils.getJsonArray(response)
+            if (concerts.length() == 0)
+                return
 
-        val concertObj = concerts.getJSONObject((0..<concerts.length()).random())
-        val concertId = concertObj.getString("id")
+            val concertObj = concerts.getJSONObject((0..<concerts.length()).random())
+            val concertId = concertObj.getString("id")
 
-        response = api.getFreeSeats(concertId).execute()
-        utils.checkValidity(response)
+            response = api.getFreeSeats(concertId).execute()
+            utils.checkValidity(response)
 
-        val seats = utils.getJsonArray(response)
+            val seats = utils.getJsonArray(response)
 
-        if (seats.length() == 0)
-            return
+            if (seats.length() == 0)
+                return
 
-        val seatObj = seats.getJSONObject((0..<seats.length()).random())
-        val row = seatObj.getInt("row")
-        val seat = seatObj.getInt("seat")
-        val username = utils.generateUsername()
+            val seatObj = seats.getJSONObject((0..<seats.length()).random())
+            val row = seatObj.getInt("row")
+            val seat = seatObj.getInt("seat")
+            val username = utils.generateUsername()
 
-        response = api.reserveSeat(concertId, row, seat, username).execute()
-        utils.checkValidity(response)
+            response = api.reserveSeat(concertId, row, seat, username).execute()
+            utils.checkValidity(response)
 
-        response = api.getMySeats(concertId, username).execute()
-        utils.checkValidity(response)
-        assert(utils.containsValues(utils.getJsonArray(response), "row",  row, "seat", seat))
+            response = api.getMySeats(concertId, username).execute()
+            utils.checkValidity(response)
+            assert(utils.containsValues(utils.getJsonArray(response), "row", row, "seat", seat))
 
-        response = api.getFreeSeats(concertId).execute()
-        utils.checkValidity(response)
-        assert(!utils.containsValues(utils.getJsonArray(response), "row", row, "seat", seat))
+            response = api.getFreeSeats(concertId).execute()
+            utils.checkValidity(response)
+            assert(!utils.containsValues(utils.getJsonArray(response), "row", row, "seat", seat))
+        }
+
+        println("Reserve test $timeTaken")
     }
 
     fun releaseSeat(){
-        var response = api.getConcerts().execute()
-        utils.checkValidity(response)
+        val timeTaken = measureTime {
+            var response = api.getConcerts().execute()
+            utils.checkValidity(response)
 
-        val concerts = utils.getJsonArray(response)
-        if (concerts.length() == 0)
-            return
+            val concerts = utils.getJsonArray(response)
+            if (concerts.length() == 0)
+                return
 
-        val concertObj = concerts.getJSONObject((0..<concerts.length()).random())
-        val concertId = concertObj.getString("id")
+            val concertObj = concerts.getJSONObject((0..<concerts.length()).random())
+            val concertId = concertObj.getString("id")
 
-        val username = utils.generateUsername()
+            val username = utils.generateUsername()
 
-        response = api.getMySeats(concertId, username).execute()
-        utils.checkValidity(response)
+            response = api.getMySeats(concertId, username).execute()
+            utils.checkValidity(response)
 
-        val reservations = utils.getJsonArray(response)
-        if (reservations.length() == 0)
-            return
+            val reservations = utils.getJsonArray(response)
+            if (reservations.length() == 0)
+                return
 
-        val reservationObj = reservations.getJSONObject((0..<reservations.length()).random())
-        val row = reservationObj.getInt("row")
-        val seat = reservationObj.getInt("seat")
+            val reservationObj = reservations.getJSONObject((0..<reservations.length()).random())
+            val row = reservationObj.getInt("row")
+            val seat = reservationObj.getInt("seat")
 
-        response = api.releaseSeat(concertId, row, seat, username).execute()
-        utils.checkValidity(response)
+            response = api.releaseSeat(concertId, row, seat, username).execute()
+            utils.checkValidity(response)
 
-        response = api.getMySeats(concertId, username).execute()
-        utils.checkValidity(response)
-        assert(!utils.containsValues(utils.getJsonArray(response), "row", row, "seat", seat))
+            response = api.getMySeats(concertId, username).execute()
+            utils.checkValidity(response)
+            assert(!utils.containsValues(utils.getJsonArray(response), "row", row, "seat", seat))
 
-        response = api.getFreeSeats(concertId).execute()
-        utils.checkValidity(response)
-        assert(utils.containsValues(utils.getJsonArray(response), "row", row, "seat", seat))
+            response = api.getFreeSeats(concertId).execute()
+            utils.checkValidity(response)
+            assert(utils.containsValues(utils.getJsonArray(response), "row", row, "seat", seat))
+        }
+
+        println("Release test $timeTaken")
     }
 }
 
